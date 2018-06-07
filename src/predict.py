5838775076e33828ca5a077880ad75b611e4c2fe
karla <karlafej@gmail.com>
2018-06-07 15:56:44 +0200

    modelfiles into params.py

--- src/predict.py
+++ src/predict.py
@@ -16,8 +16,9 @@ from params import *
 from data.dataset import TestImageDataset
 import img.transformer as transformer
 
-from data.export import export_images, create_csv, create_dirs
+from data.export import *
 import sys, getopt
+from tqdm import tqdm
 
 def main(argv):
   
@@ -25,16 +26,13 @@ def main(argv):
     img_resize = (700, 700)
     batch_size = 12
     epochs = 15
-    threshold = 0.3 # default 0.5
+    threshold = 0.5 # default 0.5
     sample_size = None # Put None to work on full dataset
-    modelfiles = {'middle': "model_middle_2018-04-17_14h37", 
-              'start': "model_start_2018-04-03_11h16",
-              'end': "model_end_2018-04-04_22h08" }
-   
+     
+
     # -- Optional parameters
-    threads = 10
-    use_cuda = torch.cuda.is_available()
-  
+    threads = 8
+    use_cuda = torch.cuda.is_available() 
 
     # -- inpath from command line
     
@@ -45,40 +43,43 @@ def main(argv):
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('argv[0] -i <inputpath> -o <outputpath>')
+            print ('argv[0] -i <inputpath>')
             sys.exit()
         elif opt in ("-i", "--input"):
             inpath = arg
             
-    CSV, dxa_lst = create_csv(inpath, datapath)
+    if mods:
+        CSV, dxa_lst, where = create_csv(inpath, datapath, mods)
 
-
+    predspath = inpath #TESTTESTTEST 
+    
     for dxa in dxa_lst:
         print("..." + dxa[-30:]) 
-        maskpath, outpath = create_dirs(dxa, predspath)
-           
+        maskpath, outpath = create_dirs(dxa, predspath, where)
+        
         if mods:
             for part in mods:
-                print(part)
+                
                 modelfile = modelfiles[part]
                 ds_fetcher = DatasetFetcher()
                 ds_fetcher.get_dataset(datapath=dxa, csv = CSV, prod = True)
                 full_x_test = ds_fetcher.get_test_files(sample_size=None, part=part, prod = True)
-               
-                img_resize_centercrop = transformer.get_center_crop_size(full_x_test[0], img_resize)
-                net = unet.UNet1024((3, *img_resize_centercrop))
+                if len(full_x_test) > 0:
+                    print(part)
+                    img_resize_centercrop = transformer.get_center_crop_size(full_x_test[0], img_resize)
+                    net = unet.UNet1024((3, *img_resize_centercrop))
                   
-                classifier = nn.classifier.ImgClassifier(net, epochs)
-                classifier.restore_model("".join([modelpath, modelfile]))
+                    classifier = nn.classifier.ImgClassifier(net, epochs)
+                    classifier.restore_model("".join([modelpath, modelfile]))
                
-                test_ds = TestImageDataset(full_x_test, img_resize)
-                test_loader = DataLoader(test_ds, batch_size,
-                                       sampler=SequentialSampler(test_ds),
-                                       num_workers=threads,
-                                       pin_memory=use_cuda)
+                    test_ds = TestImageDataset(full_x_test, img_resize)
+                    test_loader = DataLoader(test_ds, batch_size,
+                                           sampler=SequentialSampler(test_ds),
+                                           num_workers=threads,
+                                           pin_memory=use_cuda)
                  
-                pred_saver_cb = PredictionsSaverCallback(outpath=maskpath, threshold=threshold)
-                classifier.predict(test_loader, callbacks=[pred_saver_cb])
+                    pred_saver_cb = PredictionsSaverCallback(outpath=maskpath, threshold=threshold)
+                    classifier.predict(test_loader, callbacks=[pred_saver_cb])
            
         export_images(imgpath=dxa, maskpath=maskpath, outpath=outpath, num_workers=threads)
 
