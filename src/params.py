770a3a6e637c7d2779cc9ed776f2463dee821545
karla <karlafej@gmail.com>
2018-09-26 13:35:26 +0200

    new modelfiles

--- src/params.py
+++ src/params.py
@@ -8,18 +8,15 @@ predspath = "/home/fejfark/PRIMUS/data/18_lab/karla/imaging/dl/ppp/"
 mods = ["st_male_end"]
 #mods = ["male_end"]
 #mods = ["female_end"]
-<<<<<<< HEAD
-epochs = 12
-=======
 epochs = 6
->>>>>>> 0b9ffe85d4f83cb19b9c0b0c720a2db672984901
-modelfiles = {'middle': "model_middle_2018-08-03_18h29",
-              'start': "model_start_2018-08-03_16h30",
+modelfiles = {'middle': "model_middle_2018-09-25_19h18",
+              'start': "model_start_2018-09-25_15h59",
               'end': "model_end_2018-04-04_22h08",
-              'female_end': "model_female_end_2018-08-06_16h40",
-              'male_end': "model_male_end_2018-08-03_22h49",
-              'st_start': "model_st_start_2018-04-26_15h32",
-              'st_middle': "model_st_middle_2018-04-26_18h10",
+              'female_end': "model_female_end_2018-09-26_05h42",
+              'male_end': "model_male_end_2018-09-26_02h36",
+              'st_start': "model_st_start_2018-09-26_08h08",
+              'st_middle': "model_st_middle_2018-09-26_08h34",
               'st_end': "model_st_end_2018-04-27_02h38",
-              'st_male_end' : "model_st_end_2018-04-27_02h38"}
+              'st_female_end': "model_st_end_2018-04-27_02h38"
+              'st_male_end' : "model_st_male_end_2018-09-26_10h53"}
 
